e5db6b4ae271f09493773ed7d3e9a5f741c51bf2
ToaderG85 <123585550+ToaderG85@users.noreply.github.com>
2023-08-15 21:53:08 +0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-import Main from "components/Main/Main";
+
+import user from '../src/data/user.json';
+import data from '../src/data/data.json';
+import friends from '../src/data/friends.json';
+import transactions from '../src/data/transactions.json';
+
+import FriendList from "../src/components/FriendList/FriendList";
+import Profile from "../src/components/Profile/Profile";
+import Statistics from '../src/components/Statistics/Statistics';
+import TransactionHistory from '../src/components/TransactionHistory/TransactionHistory';
+
 
 export const App = () => {
   return (
-    <div> 
-      <Main />     
+    <div className="layout">
+        <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+        />
+        <Statistics 
+            title="Upload stats" 
+            stats={data} 
+        />
+        <FriendList 
+            friends={friends}            
+        />
+        <TransactionHistory items={transactions}/>
+
     </div>
   );
 };
